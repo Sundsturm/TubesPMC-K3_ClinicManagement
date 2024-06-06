57bc51f98d2fdcc86ec080133e0c7c4b4870475d
Kean Malik <keanmalik92@gmail.com>
2024-06-06 23:39:30 +0700

    Create program utama (main.c)

---
+++ main.c
@@ -0,0 +1,21 @@
+//EL2008 - Pemecahan Masalah dengan C 2023/2024
+//Topik				: Tugas Besar - Clinic Management
+//Kelompok/Kelas	: 03/02
+//Nama File			: main.c
+//Deskripsi			: Program utama atau main hub dari aplikasi manajemen data klinik
+
+// Import beberapa header pendukung aplikasi
+// Pembuatan prosedur dan fungsi pendukung dibuat terpisah dengan file ini
+
+
+// Definisi macros dan list yang dipakai
+
+
+// Fungsi/prosedur untuk program utama
+
+
+// Program utama
+int main() {
+
+
+}
