4f389b7ba43a58d8e2e74c7a915094eee22a6a46
Kean Malik <keanmalik92@gmail.com>
2024-06-08 20:10:30 +0700

    Fix segmentation faults on libworkwithdata.c functions/procedures

--- lib/src/libworkwithdata.c
+++ lib/src/libworkwithdata.c
@@ -4,89 +4,90 @@
 #include "../include/workwithdata.h"
 
 /* Prosedur untuk meng-assign tanggal, bulan, dan tahun ke */
-void assignTanggal(char* str_t, Date* tanggal_t) {
-	char *token;
-	int num_t;
+void assignTanggal(char *str_t, Date* tanggal_t) {
+	char *saveptr;
+	char *temp_string;
+	
+	// Membaca hari
+	temp_string = strtok_r(str_t, " ", &saveptr);
+	tanggal_t->hari = atoi(temp_string);
 
-	token = strtok(str_t, " ");
-	num_t = atoi(token);
-	tanggal_t->hari = num_t;
+	// Membaca bulan
+	temp_string = strtok_r(NULL, " ", &saveptr);
+	strcpy(tanggal_t->bulan, temp_string);
 
-	token = strtok(NULL, " ");
-	strcpy(tanggal_t->bulan, token);
-
-	token = strtok(NULL, " ");
-	num_t = atoi(token);
-	tanggal_t->tahun = num_t;
+	// Membaca tahun
+	temp_string = strtok_r(NULL, " ", &saveptr);
+	tanggal_t->tahun = atoi(temp_string);
 }
-
+ 
 /* Prosedur untuk membaca CSV yang menyimpan data pasien */
-void readCSVPasien(char *file, char *del, Pasien** list_pt) {
+void readCSVPasien(char *file, Pasien** list_pt) {
 	/* Definisi variabel dan data yang dipakai untuk pembacaan CSV*/
 	char* token;
 	char lines[MAX_STRING];
+	char del[2] = ";";
 	Pasien* temp = NULL;
-	
+ 
 	/* Membuka file */
 	FILE* file_path = fopen(file, "r");
-
+ 
 	/* Cek kebenaran file input */
 	if (!file_path) {
 		printf("File tidak bisa dibuka.\n");
 		return;
 	}
-
+ 
 	/* Membaca baris pertama yang berisi keterangan data CSV*/
 	fgets(lines, MAX_STRING, file_path);
 	strtok(lines, del);
-
+ 
 	/* Membaca data dengan loop */
 	while (fgets(lines, MAX_STRING, file_path)) {
 		/* Variabel sementara untuk membaca data baru*/
 		Pasien* read_data = malloc(sizeof(*read_data));
-		Date *temp_tanggal = malloc(sizeof(*temp_tanggal));
-		char* temp_str[MAX_STRING] = NULL;
-		int temp_num = 0;
+		Date temp_tanggal;
+
+		/* Pembacaan data dan assign data ke read_data*/
+		// Nomor
+		token = strtok(lines, del);
+		read_data->nomor = atoi(token);
+
+		// Nama Lengkap
+		token = strtok(NULL, del);
+		strcpy(read_data->nama_lengkap, token);
+
+		// Alamat
+		token = strtok(NULL, del);
+		strcpy(read_data->alamat, token);
+
+		// Kota
+		token = strtok(NULL, del);
+		strcpy(read_data->kota, token);
 		
+		// Tempat Lahir
+		token = strtok(NULL, del);
+		strcpy(read_data->tempat_lahir, token);
+
+		// Tanggal Lahir
+		token = strtok(NULL, del);
+		assignTanggal(token, &temp_tanggal);
+		read_data->tanggal_lahir = temp_tanggal;
+
+		// Umur
+		token = strtok(NULL, del);
+		read_data->umur = atoi(token);
+
+		// BPJS
+		token = strtok(NULL, del);
+		strcpy(read_data->bpjs, token);
+
+		// ID Pasien
+		token = strtok(NULL, del);
+		strcpy(read_data->id_pasien, token);
+
 		/* Membaca data baris pertama */
 		if (*list_pt == NULL) {
-			token = strtok(lines, del);
-			temp_num = atoi(token);
-			read_data->nomor = temp_num;
-
-			token = strtok(NULL, del);
-			strcpy(temp_str, token);
-			strcpy(read_data->nama_lengkap, temp_str);
-
-			token = strtok(NULL, del);
-			strcpy(temp_str, token);
-			strcpy(read_data->alamat, temp_str);
-
-			token = strtok(NULL, del);
-			strcpy(temp_str, token);
-			strcpy(read_data->kota, temp_str);
-
-			token = strtok(NULL, del);
-			strcpy(temp_str, token);
-			strcpy(read_data->tempat_lahir, temp_str);
-
-			token = strtok(NULL, del);
-			strcpy(temp_str, token);
-			assignTanggal(temp_str, temp_tanggal);
-			read_data->tanggal_lahir = *temp_tanggal;
-			
-			token = strtok(NULL, del);
-			temp_num = atoi(token);
-			read_data->umur = temp_num;
-
-			token = strtok(NULL, del);
-			strcpy(temp_str, token);
-			strcpy(read_data->bpjs, temp_str);
-
-			token = strtok(NULL, del);
-			strcpy(temp_str, token);
-			strcpy(read_data->id_pasien, temp_str);
-
 			read_data->next = NULL;
 			read_data->prev = NULL;
 			*list_pt = read_data;
@@ -95,117 +96,76 @@ void readCSVPasien(char *file, char *del, Pasien** list_pt) {
 		/* Membaca data untuk baris-baris selanjutnya*/
 		else {
 			while (temp->next != NULL) temp = temp->next;
-
-			token = strtok(lines, del);
-			temp_num = atoi(token);
-			read_data->nomor = temp_num;
-
-			token = strtok(NULL, del);
-			strcpy(temp_str, token);
-			strcpy(read_data->nama_lengkap, temp_str);
-
-			token = strtok(NULL, del);
-			strcpy(temp_str, token);
-			strcpy(read_data->alamat, temp_str);
-
-			token = strtok(NULL, del);
-			strcpy(temp_str, token);
-			strcpy(read_data->kota, temp_str);
-
-			token = strtok(NULL, del);
-			strcpy(temp_str, token);
-			strcpy(read_data->tempat_lahir, temp_str);
-
-			token = strtok(NULL, del);
-			strcpy(temp_str, token);
-			assignTanggal(temp_str, temp_tanggal);
-			read_data->tanggal_lahir = *temp_tanggal;
-			
-			token = strtok(NULL, del);
-			temp_num = atoi(token);
-			read_data->umur = temp_num;
-
-			token = strtok(NULL, del);
-			strcpy(temp_str, token);
-			strcpy(read_data->bpjs, temp_str);
-
-			token = strtok(NULL, del);
-			strcpy(temp_str, token);
-			strcpy(read_data->id_pasien, temp_str);
-
 			read_data->prev = temp;
 			temp->next = read_data;
 			read_data->next = NULL;
 		}
-
-		free(&(read_data->tanggal_lahir));
-		free(read_data);
 	}
 
 	/* Menutup file setelah pembacaan*/
-	fclose(file);
+	fclose(file_path);
 }
-
+ 
 /* Prosedur untuk membaca CSV yang menyimpan data diagnosis pasien */
-void readCSVDiagnosis(char* file, char* del, Diagnosis** list_dx) {
+void readCSVDiagnosis(char* file, Diagnosis** list_dx) {
 	/* Definisi variabel dan data yang dipakai untuk pembacaan CSV*/
 	char* token;
 	char lines[MAX_STRING];
+	char del[2] = ";";
 	Diagnosis* temp = NULL;
-
+ 
 	/* Membuka file */
 	FILE* file_path = fopen(file, "r");
-
+ 
 	/* Cek kebenaran file input */
 	if (!file_path) {
 		printf("File tidak bisa dibuka.\n");
 		return;
 	}
-
+ 
 	/* Membaca baris pertama yang berisi keterangan data CSV*/
 	fgets(lines, MAX_STRING, file_path);
 	strtok(lines, del);
-
+ 
 	/* Membaca data dengan loop */
 	while (fgets(lines, MAX_STRING, file_path)) {
 		/* Variabel sementara untuk membaca data baru*/
 		Diagnosis* read_data = malloc(sizeof(*read_data));
-		Date *temp_tanggal = malloc(sizeof(Date));
-		char* temp_str[MAX_STRING] = NULL;
-		int temp_num = 0;
+		Date temp_tanggal;
+		
+		/* Membaca data dan assign ke read_data */
+		// Nomor
+		token = strtok(lines, del);
+		read_data->nomor = atoi(token);
+
+		// Tanggal Cek
+		token = strtok(NULL, del);
+		assignTanggal(token, &temp_tanggal);
+		(read_data)->tanggal_cek = temp_tanggal;
+
+		// ID Pasien
+		token = strtok(NULL, del);
+		strcpy(read_data->id_pasien, token);
+
+		// Diagnosis
+		token = strtok(NULL, del);
+		strcpy(read_data->diagnosis, token);
+
+		// Tindakan
+		token = strtok(NULL, del);
+		strcpy(read_data->tindakan, token);
+
+		// Tanggal Kontrol
+		token = strtok(NULL, del);
+		assignTanggal(token, &temp_tanggal);
+		read_data->tanggal_kontrol = temp_tanggal;
+
+		// Biaya
+		token = strtok(NULL, del);
+		read_data->biaya = atoi(token);
 
 		/* Membaca data baris pertama */
 		if (*list_dx == NULL) {
-			token = strtok(lines, del);
-			temp_num = atoi(token);
-			read_data->nomor = temp_num;
-
-			token = strtok(NULL, del);
-			strcpy(temp_str, token);
-			assignTanggal(temp_str, temp_tanggal);
-			(read_data)->tanggal_cek = *temp_tanggal;
-
-			token = strtok(NULL, del);
-			strcpy(temp_str, token);
-			strcpy(read_data->id_pasien, temp_str);
-
-			token = strtok(NULL, del);
-			strcpy(temp_str, token);
-			strcpy(read_data->diagnosis, temp_str);
-
-			token = strtok(NULL, del);
-			strcpy(temp_str, token);
-			strcpy(read_data->tindakan, temp_str);
-
-			token = strtok(NULL, del);
-			strcpy(temp_str, token);
-			assignTanggal(temp_str, temp_tanggal);
-			read_data->tanggal_kontrol = *temp_tanggal;
-
-			token = strtok(NULL, del);
-			temp_num = atoi(token);
-			read_data->biaya = temp_num;
-
 			read_data->next = NULL;
 			read_data->prev = NULL;
 			*list_dx = read_data;
@@ -213,50 +173,15 @@ void readCSVDiagnosis(char* file, char* del, Diagnosis** list_dx) {
 		}
 		/* Membaca data untuk baris-baris selanjutnya*/
 		else {
-			while (temp->next != NULL) temp = temp->next;
-
-			token = strtok(lines, del);
-			temp_num = atoi(token);
-			read_data->nomor = temp_num;
-
-			token = strtok(NULL, del);
-			strcpy(temp_str, token);
-			assignTanggal(temp_str, temp_tanggal);
-			(read_data)->tanggal_cek = *temp_tanggal;
-
-			token = strtok(NULL, del);
-			strcpy(temp_str, token);
-			strcpy(read_data->id_pasien, temp_str);
-
-			token = strtok(NULL, del);
-			strcpy(temp_str, token);
-			strcpy(read_data->diagnosis, temp_str);
-
-			token = strtok(NULL, del);
-			strcpy(temp_str, token);
-			strcpy(read_data->tindakan, temp_str);
-
-			token = strtok(NULL, del);
-			strcpy(temp_str, token);
-			assignTanggal(temp_str, temp_tanggal);
-			read_data->tanggal_kontrol = *temp_tanggal;
-
-			token = strtok(NULL, del);
-			temp_num = atoi(token);
-			read_data->biaya = temp_num;
-
+			while (temp->next != NULL) temp = temp->next; 
 			read_data->prev = temp;
 			temp->next = read_data;
 			read_data->next = NULL;
 		}
-
-		free(&(read_data->tanggal_cek));
-		free(&(read_data->tanggal_kontrol));
-		free(read_data);
 	}
-
+ 
 	/* Menutup file setelah pembacaan*/
-	fclose(file);
+	fclose(file_path);
 }
 
 void printPasien(Pasien *list_pt){
